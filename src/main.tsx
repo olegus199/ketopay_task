7638bb5dabe3e1fe5bbd775a1f2353159cf7f478
olegus199 <okremnyov@yandex.ru>
2025-03-03 12:33:00 +0300

    feat: add react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import "./index.scss";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./state/store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
