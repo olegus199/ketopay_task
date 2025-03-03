c2df87a896c2a8b07b92522a03ac51bc859e95d2
olegus199 <okremnyov@yandex.ru>
2025-03-03 15:35:29 +0300

    chore: update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,2 @@
-// export const API_KEY = "36624371ae7e4822b9a25fd5af5add29";
-// export const API_KEY = "a860e526e2234c87a0e93315ee1e31cf";
 export const API_KEY = "daf4fb3d0a8749d6bff179c7307824e1";
 export const API_URL = "https://newsapi.org/v2/everything";
-// export const API_KEY = "WcYtf0gQzAWemDZdevNAJWTJmFefGfgY";
-// export const API_URL = "https://api.nytimes.com/svc/archive/v1/2024/1.json";
